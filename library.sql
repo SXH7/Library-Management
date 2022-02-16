fd5e8a1a6aae989340b9f5591b06852233dde2c6
Pratyush <karmasama123456@gmail.com>
2022-02-16 19:37:33 +0530

    yo

--- library.sql
+++ library.sql
@@ -5,7 +5,7 @@ use library;
 create table member(Mem_ID char(4) NOT NULL PRIMARY KEY, 
                     Mem_Name varchar(30) NOT NULL, 
                     Mem_DOB date NOT NULL, 
-                    Mem_Phone int(11) NOT NULL, 
+                    Mem_Phone bigint(11) NOT NULL, 
                     Mem_address varchar(40) NOT NULL,
                     Mem_perms varchar(5) NOT NULL CHECK(Mem_perms = "Staff" or Mem_perms = "User"));
 
--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 from asyncore import read
 from pydoc import importfile
+from select import select
 import mysql.connector as i
+from datetime import datetime
 
-log=i.connect(host="localhost",user="root",password="shashwat",database="library_management_program")
+log=i.connect(host="localhost",user="root",password="remon",database="library")
 
-def addbook():
-    bn=input("Enter the book name:")
-    c=input("Enter the book code:")
-    t=input("Enter the total number of books:")
-    s=input("Enter the subject of books:")
-    data = (bn,c,t,s)
-    sql='insert into book values(%s,%s,%s,%s)'
+def book():
+
+    c=log.cursor()
+    c.execute("select * from books")
+    result= c.fetchall()
+    for row in result:
+        print(row)
+        print("\n")
+
+
+def memberadd():
+    mi=input("Enter member id given by admin:")
+    
+    n=input("Enter your name:")
+    print("Enter the date in sequence of MM-DD-YYYY")
+    d=(input("Enter the date:"))
+
+    dat=datetime.strptime(d, '%m-%d-%Y')
+    
+    
+    phno=int(input("Enter your phone no.:"))
+
+    ader=input("Enter your address:")
+
+    wr=input("Enter if you are a user or staff:")
+    
+    data=(mi,n,dat,phno,ader,wr)
+    print(data)
+    sql=("insert into member values (%s,%s,%s,%s,%s,%s)")
     c=log.cursor()
     c.execute(sql,data)
     log.commit()
-    print("Book(s) Added sucessfully")
-    print("------------------------------------------------------------------")
 
-def issueb():
-    ina=input("Enter name:")
-    reg=input("Enter registration no.:")
-    bco=input("Enter book code:")
-    isu=input("Enter Date:")
-    data=(ina,reg,bco,isu)
-    sql='insert into issue values(%s,%s,%s,%s)'
-    c=log.cursor()
-    c.execute(sql,data)
-    log.commit()
-    print("Issue done sucessfully")
-    print("------------------------------------------------------------------")
-    bookup=(bco,-1)
+    print("Member added successfully")
 
-def summitb():
-    ina=input("Enter name:")
-    reg=input("Enter registration no.:")
-    bco=input("Enter book code:")
-    isu=input("Enter Date:")
-    data=(ina,reg,bco,isu)
-    sql='insert into summit values(%s,%s,%s,%s)'
-    c=log.cursor()
-    c.execute(sql,data)
-    log.commit()
-    print("Submission done sucessfully")
-    print("------------------------------------------------------------------")
-    bookup=(bco,+1)
+    
+memberadd()
+    
+    
+
+
+   
